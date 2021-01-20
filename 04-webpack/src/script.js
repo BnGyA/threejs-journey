88963f7a62465107685f1523dc37047fabdd1a2e
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-20 21:15:09 +0100

    Refactoring the code

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -11,12 +11,12 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 //Camera
-const sizes = { 
+const sizes = {
     width: 800,
     height: 600
 }
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3 
+camera.position.z = 3   
 scene.add(camera)
 
 
