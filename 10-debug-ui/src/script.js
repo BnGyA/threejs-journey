508167b47a8d5250d7cbbec4d4827a0027663536
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-01-22 11:36:37 +0100

    Lesson 10 - dat.gui added

---
+++ 10-debug-ui/src/script.js
@@ -0,0 +1,119 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
+const debugObject = {
+    color: 0xff0000,
+    spin: () =>{
+        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
+
+    }
+}
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+/**
+ * DATGUI
+ */
+//const gui = new dat.GUI({ closed: true })
+//const gui = new dat.GUI({ width: 400 })
+const gui = new dat.GUI()
+
+
+
+gui.add(mesh.position, 'y', -3, 3, 0.01)
+gui.add(mesh.position, 'x', -3, 3, 0.01)
+gui.add(mesh.position, 'z', -3, 3, 0.01)
+gui.add(mesh, 'visible')
+console.log(mesh)
+
+gui.add(material, 'wireframe')
+gui
+    .addColor(debugObject, 'color')
+    .onChange(() =>{
+        material.color.set(debugObject.color)
+    })
+
+gui.add(debugObject, 'spin')
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
