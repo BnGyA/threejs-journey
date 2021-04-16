e05437e793fc8f3fc2e52a620e355fe12d03ffa3
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-04-16 15:01:00 +0200

    Optimized code

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -205,17 +205,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Utils
  */
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20);
+const sphereMaterial =  new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+});
+
 const objectsToUpdate = [];
 const createSphere = (radius, position) =>{
     // THREEJS mesh
-    const mesh = new THREE.Mesh(
-        new THREE.SphereBufferGeometry(radius, 20, 20),
-        new THREE.MeshStandardMaterial({
-            metalness: 0.3,
-            roughness: 0.4,
-            envMap: environmentMapTexture
-        })
-    )
+    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    mesh.scale.set(radius, radius, radius);
     mesh.castShadow = true;
     mesh.position.copy(position);
     scene.add(mesh);
